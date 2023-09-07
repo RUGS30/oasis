ece7adadeb5267a2fa6e5093ef09b188f0594db6
Raul Garcia <rugs_99@outlook.com>
2023-09-07 13:33:36 -0600

    Avance Proyecto

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,19 +1,30 @@
-import { useEffect } from "react";
+
+
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
+import { useState } from "react";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm"
+
 
 function Cabins() {
-  
-  useEffect(function(){
-    getCabins().then((data) => console.log(data));
-  }, []);
 
+  const [showForm,setShowForm] = useState(false)
+  
   return (
+    <>
     <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+      <Heading as="h1">Cabañas</Heading>
+      <p>Filtrar / Sort</p>
     </Row>
+
+    <Row>
+      <CabinTable/>
+      <Button onClick={() =>setShowForm(show=>!show)} >Agregar nueva cabaña</Button>
+      {showForm && <CreateCabinForm />}  
+    </Row>
+    </>
   );
 }
 
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,14 +1,32 @@
 import supabase from "./supabase";
 
-export async function getCabins(){
-    
-const { data, error } = await supabase.from("cabins").select('*');
 
-if(error){
-    console.error(error);
-    throw new Error('Cabins no pudieron ser recibidas');
-}
 
-return data;
+export async function getCabins() {
+    const { data, error } = await supabase.from("cabins").select("*");
+  
+    if (error) {
+      console.error(error);
+      throw new Error("Las Cabañas no se pudieron cargar ");
+    }
+  
+    return data;
+  }
+  
 
-}
+export async function deleteCabin(id){
+  
+      const { data,error } = await supabase
+      .from('cabins')
+      .delete()
+      .eq('id', id)
+
+
+    if (error) {
+      console.error(error);
+      throw new Error("No se pudo eliminar la Cabaña");
+    }
+  
+    return data;
+
+  }
